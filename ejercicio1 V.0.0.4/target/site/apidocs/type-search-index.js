16d6ad035c588a48e687aa2ad073d5b4d6ee9a74
Diego Cordero Contreras <92685068+Bersaglieri412@users.noreply.github.com>
2022-12-22 04:25:52 +0100

    Revert "Mantenimiento"

--- ejercicio1 V.0.0.4/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.mohacarlos.ejercicio1","l":"ExcepcionFecha"},{"p":"com.mohacarlos.ejercicio1","l":"Fecha"}];updateSearchResults();
--- ejercicio1 V.0.0.4/target/site/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.mohacarlos.ejercicio1","l":"ExcepcionFechaTest"},{"p":"com.mohacarlos.ejercicio1","l":"FechaTest"}];updateSearchResults();
